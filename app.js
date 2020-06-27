7e36e407e6b78805d41d80f64d53e36046961b05
nimra555 <rosegulab505@gmail.com>
2020-06-27 23:05:24 +0500

    update files

--- app.js
+++ app.js
@@ -4,21 +4,15 @@ function getNumber(num){
 }
 function clearResult(){
     var result = document.getElementById("result");
-    result.value = 0
-    if(result.value==7){
-        result.value = ""
-        console.log("check result")
-    }
-    else{
-        console.log("wrong")
-    }
+    result.value = "";
 }
-function getResult(){
+function sumResult(){
     var result = document.getElementById("result");
-    result.value = eval(result.value);
+    return(
+    result.value = eval(result.value)
+    )
 }
 function backResult(){
     var result = document.getElementById("result");
-    result.value = result.value.charAt(result.lenght - 1 );
-    console.log(result.value.charAt(result.value.lenght - 1  ));
+    result.value = result.value.slice(0,-1);     
 }
